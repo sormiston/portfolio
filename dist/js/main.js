1814db71f08409d83f5b92d1361cd4d2f3d36485
SOrmiston <sean.david.ormiston@gmail.com>
2020-09-30 22:52:15 -0400

    site v1

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,36 +7,40 @@ const navItems = document.querySelectorAll('.nav-item')
 
 // Set Initial State of Menu
 
-let showMenu = false;
+let showMenu = false
 
-menuBtn.addEventListener('click', toggleMenu);
+menuBtn.addEventListener('click', toggleMenu)
 
 function toggleMenu() {
-  
   if (!showMenu) {
-    menuBtn.classList.add('close');
-    menu.classList.add('show');
-    menuNav.classList.add('show');
-    menuBranding.classList.add('show');
-    navItems.forEach(item => item.classList.add('show'))
-    
+    menuBtn.classList.add('close')
+    menu.classList.add('show')
+    menuNav.classList.add('show')
+    menuBranding.classList.remove('close')
+    menuBranding.classList.add('show')
+    navItems.forEach((item) => item.classList.add('show'))
+
     // Set Menu State
     showMenu = true
   } else {
-    menuBtn.classList.remove('close');
-    menu.classList.remove('show');
-    menuNav.classList.remove('show');
-    menuBranding.classList.remove('show');
-    navItems.forEach(item => item.classList.remove('show'))
-    
+    menuBtn.classList.remove('close')
+    menu.classList.remove('show')
+    menuNav.classList.remove('show')
+    menuBranding.classList.remove('show')
+    menuBranding.classList.add('close')
+    navItems.forEach((item) => item.classList.remove('show'))
+
     // Set Menu State
     showMenu = false
   }
-  
 }
 // for li.nav-item in navItems, add eventListener
-navItems.forEach(item => item.addEventListener('click', (e) => select(e.target)))
-navItems.forEach(item => item.addEventListener('mouseover', (e) => navigate(e.target)))
+navItems.forEach((item) =>
+  item.addEventListener('click', (e) => select(e.target))
+)
+navItems.forEach((item) =>
+  item.addEventListener('mouseover', (e) => navigate(e.target))
+)
 
 //define callback function that removes current from previous and adds to selected
 function navigate(elt) {
@@ -48,6 +52,3 @@ function navigate(elt) {
 function select() {
   setTimeout(toggleMenu, 500)
 }
-
-
-
